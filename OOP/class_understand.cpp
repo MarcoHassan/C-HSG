b1db474b3d7d0a900d994492bbd40b470939b668
MarcoHassan <marco.hassan30@gmail.com>
2019-03-14 15:00:59 +0100

    completation of 3 intro exercises on classes

--- OOP/class_understand.cpp
+++ OOP/class_understand.cpp
@@ -6,20 +6,27 @@ Go through the below code and explain in few lines of text what you understood f
 #include <iostream>
 using namespace std;
 
-class Person
+class Person // define an object person.
 {
-public:
-	Person()
+public: 
+        Person() /* constructor to initialize the the object and assign 
+		    the necessary memory space to it. */
 	{
 		cout  << "Default constructor of base class called" << endl;
-	}
-	Person(string lName, int year)
+	} // feedback message above.
+  
+	Person(string lName, int year) /* constructor already initializing
+					  the object length and 
+					  yearOfBirth */
 	{
-		cout  << "Parameterized constructor of base class called" << endl;
-		lastName = lName;
-		yearOfBirth = year;
+	  cout  << "Parameterized constructor of base class called";
+	  cout << endl;
+	  lastName = lName;
+	  yearOfBirth = year;
 	}
-	string lastName;
+
+        // Specifying public member variables
+        string lastName;
 	int yearOfBirth;
 };
 
@@ -27,11 +34,15 @@ public:
 class Student :public Person
 {
 public:
+        // specify base constructor for Student objects. 
 	Student()
 	{
 		cout  << "Default constructor of Derived class called" << endl;
 	}
+
+        // Specify parameterized constructor for Student objects. 
 	Student(string lName, int year, string univer)
+	//:Person(lName, year) // my addition to initialize the last Name and year based on the base constructor.
 	{
 		cout  << "Parameterized constructor of Derived class called" << endl;
 		university  = univer;
@@ -41,7 +52,11 @@ public:
 
 int main ()
 {
-Student student1; //Using default constructor of Student class
-Student student2("John", 1990, "London School of  Economics"); //calling parameterized constructor of Student class
-	return 0;
+       Student student1; //Using default constructor of Student class
+       Student student2("John", 1990, "London School of  Economics"); //calling parameterized constructor of Student class
+       // C++ recognizes which one your are calling based  on the overloading principle similar to the one of function declaration.
+
+       // This final line is my test. At the current stage the lName and year variables are not initialized. In order to change that you have to remove the commentary from the Subclass constructor above where the base constructor is referred.
+       cout<<  student2.lastName <<endl;
+       return 0;
 }
