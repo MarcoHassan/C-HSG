3aebac4d076ab13ea547b52a404aff478e64e82d
MarcoHassan <marco.hassan30@gmail.com>
2019-03-31 18:54:07 +0200

    finished OOP small exercises

--- OOP/class_Zoo.cpp
+++ OOP/class_Zoo.cpp
@@ -1,4 +1,5 @@
-/*The ZooAnimal class definition below is missing a prototype for
+/*
+The ZooAnimal class definition below is missing a prototype for
 the Create function.  It should have parameters so that a character
 string and three integer values (in that order) can be provided when
 it is called for a ZooAnimal object.  Like the Destroy function, it
@@ -6,19 +7,40 @@ should have return type void.  Write an appropriate prototype for the
 ZooAnimal Create function.
 */
 #include <iostream>
-   class ZooAnimal  
-   {
-    private:
-      char *name;
-      int cageNumber;
-      int weightDate;
-      int weight;
-    public:
-      void Destroy (); // destroy function
-      char* reptName ();
-      int daysSinceLastWeighed (int today);
-   };
+#include <cstring> /* for strlen and strcpy */
+
+using namespace std;
+
+class ZooAnimal  
+ {
+  private:
+    char *name;
+    int cageNumber;
+    int weightDate;
+    int weight;
+   
+  public:
+    void Create (char*, int, int, int);
+    void Destroy (); // destroy function
+    char* reptName ();
+    int daysSinceLastWeighed (int today);
+ };
    
 int main() {
   std::cout << "Hello World!\n";
+
+  ZooAnimal cow;
+
+  cow.Create((char*)"pah-RAH", 12, 1998, 300);
+
+  return 0;
+}
+
+void ZooAnimal::Create(char* shem, int mispar, int date, int miskal)
+{
+  name = new char[strlen(shem)+1];
+  strcpy(name, shem);
+  cageNumber = mispar;
+  weightDate = date;
+  weight = miskal;
 }
