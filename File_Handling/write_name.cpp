1899bf6a46ccc0e9beb721dc939e1a0e8984c172
MarcoHassan <marco.hassan30@gmail.com>
2019-04-02 14:35:40 +0200

    ordered folder

--- File_Handling/write_name.cpp
+++
@@ -1,7 +0,0 @@
-// Can you write a program which write bytes into the file test.dat, so it writes your name in the file and then reads your name that is then displayed?
-
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World!\n";
-}
